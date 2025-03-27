e752666d50ba19a1605e0be91119ee100d92edc3
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-03-27 12:40:30 +0300

    Faet: update the dashbaord page

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -4,22 +4,27 @@ import TransactionManager from "../components/transactions/TransactionManager";
 // import BlockchainStats from "../components/blockchain/BlockchainStats";
 
 const DashboardPage: React.FC = () => {
-    const { user } = useAuth();
+  const { user } = useAuth();
 
-    return (
-        <div className="dashboard-page">
-            <div className="dashboard-header">
-            <h1 style={{ fontSize: "2.5rem", marginTop: "2rem", marginBottom: "5rem", padding: "1rem 1rem" }}>
-                   
-                </h1>
-            </div>
+  return (
+    <div className="dashboard-page">
+      <div className="dashboard-header">
+        <h1
+          style={{
+            fontSize: "2.5rem",
+            marginTop: "2rem",
+            marginBottom: "5rem",
+            padding: "1rem 1rem",
+          }}
+        ></h1>
+      </div>
 
-            <div className="dashboard-content">
-                {/* Display blockchain-related stats */}
-                <TransactionManager /> {/* Manage transactions */}
-            </div>
-        </div>
-    );
+      <div className="dashboard-content">
+        {/* Display blockchain-related stats */}
+        <TransactionManager /> {/* Manage transactions */}
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
