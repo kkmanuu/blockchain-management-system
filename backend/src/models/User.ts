c0b3d5c4d0d5206b661fbd5cbc3e1cc50a15fc0e
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-04-14 20:23:51 +0300

    Feat: format the code

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,19 +1,19 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 export interface IUser extends Document {
-    _id: mongoose.Types.ObjectId;
-    name: string;
-    email: string;
-    password: string;
+  _id: mongoose.Types.ObjectId;
+  name: string;
+  email: string;
+  password: string;
 }
 
 const UserSchema = new Schema<IUser>(
-    {
-        name: { type: String, required: true },
-        email: { type: String, unique: true, required: true },
-        password: { type: String, required: true },
-    },
-    { timestamps: true }
+  {
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
 );
 
 const User = mongoose.model<IUser>("User", UserSchema);
