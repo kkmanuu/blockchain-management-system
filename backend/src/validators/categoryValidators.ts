b5306f859d9269b2bd5efb9011b439712ce25379
kkmanuu <kipngenoemmanuel479@gmail.com>
2025-04-15 20:24:01 +0300

    feat: update  the code

--- backend/src/validators/categoryValidators.ts
+++ backend/src/validators/categoryValidators.ts
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
 export const categorySchema = Joi.object({
-    name: Joi.string().required(),
-    type: Joi.string().valid("income", "expense", "any").default("any"),
+  name: Joi.string().required(),
+  type: Joi.string().valid("income", "expense", "any").default("any"),
 });
